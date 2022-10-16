e108db25b957fe3b5d1d6cd83aaaf1b1750ed245
Klementiev Daniil <Klem_em31@student.itstep.org>
2022-10-16 12:39:57 +0400

    Add project files.

---
+++ ColorPractice/ColorVM.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ColorPractice
+{
+    internal class ColorViewModel : INotifyPropertyChanged
+    {
+        public Color model;
+        public ObservableCollection<ListBoxItem> colorList = new();
+        public ColorViewModel()
+        {
+            model = new Color { alpha = 0, red = 0, green = 0, blue = 0 };
+        }
+        public int Alpha
+        {
+            get { return model.alpha; }
+            set
+            {
+                model.alpha = value;
+                OnPropertyChanged("Alpha");
+            }
+        }
+        public int Red
+        {
+            get { return model.red; }
+            set
+            {
+                model.red = value;
+                OnPropertyChanged("Red"); 
+            }
+        }
+        public int Green
+        {
+            get { return model.green; }
+            set
+            {
+                model.green = value;
+                OnPropertyChanged("Green");
+            }
+        }
+        public int Blue
+        {
+            get { return model.blue; }
+            set
+            {
+                model.blue = value;
+                OnPropertyChanged("Blue");
+            }
+        }
+
+
+        // Event binds view to ViewModel
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                this.PropertyChanged(this, e);
+            }
+        }
+    }
+}
---
+++ ColorPractice/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+
+namespace ColorPractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ColorViewModel VMColor;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = VMColor;
+            alphaSlider.Value = 0;
+            redSlider.Value = 0;
+            greenSlider.Value = 0;
+            blueSlider.Value = 0;
+        }
+
+        private void redSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            box.Background = new SolidColorBrush(System.Windows.Media.Color.FromArgb((byte)VM.Alpha, (byte)VM.Red, (byte)VM.Green, (byte)VM.Blue));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var data = DataContext as ColorViewModel;
+            data.colorList.Add(new ListBoxItem() { Content = data.model, Background = new SolidColorBrush() { Color = System.Windows.Media.Color.FromArgb((byte)data.Alpha, (byte)data.Red, (byte)data.Green, (byte)data.Blue) } });
+            listbox.Items.Add(VM.colorList[VM.colorList.Count - 1]);
+        }
+    }
+}
